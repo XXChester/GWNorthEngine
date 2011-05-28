63236404433b77e29adae0e056f1655cff5e55bf
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2011-05-28 23:13:28 +0000

    Added in the ability to add tools to the engine and more importantly contain their constant values so they are in one place and sub programs using their output does not have to duplicate the values. Also created a Translator responsible for handling the translation between tool values and other values such as a TilePlacer's value to an A* value.
    git-svn-id: http://gwnp-server/repos/Programming/Team%20Work/GWNorthEngine@275 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

---
+++ Tools/TilePlacer/TileValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GWNorthEngine.Tools.TilePlacer {
+	public enum TileValues {
+		Unknown = -1,
+		BackGround = 0,
+		NoMovements = 1,
+		StandOns = 2,
+		Climbers = 2
+	}
+}
---
+++ Tools/Translator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GWNorthEngine.AI.AStar;
+using GWNorthEngine.Tools.TilePlacer;
+using GWNorthEngine.Utils;
+namespace GWNorthEngine.Tools {
+	/// <summary>
+	/// Translation utility between tool values and other engine components values
+	/// </summary>
+	public static class Translator {
+		/// <summary>
+		/// Translates am int representation of a TilePlacer.TileValue to an AI.AStar.PathFinder.TypeOfSpace value
+		/// </summary>
+		/// <param name="value">Tile placer value</param>
+		/// <returns>AStar representation of the value passed in</returns>
+		public static PathFinder.TypeOfSpace translateTileValueToAStarType(int value) {
+			PathFinder.TypeOfSpace aStarValue;
+			TileValues tileValue = EnumUtils.numberToEnum<TileValues>(value);
+			switch (tileValue) {
+				case TileValues.BackGround:
+					aStarValue = PathFinder.TypeOfSpace.Unwalkable;
+					break;
+				case TileValues.NoMovements:
+					aStarValue = PathFinder.TypeOfSpace.Unwalkable;
+					break;
+				default:
+					aStarValue = PathFinder.TypeOfSpace.Walkable;
+					break;
+			}
+			return aStarValue;
+		}
+	}
+}
