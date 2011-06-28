2345e545adb586881f853ce46a70d23cc639d80d
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2011-06-28 21:54:57 +0000

    Added in constant values used for the loading and saving of files created via the TilePlacer tool
    git-svn-id: http://gwnp-server/repos/Programming/Team%20Work/GWNorthEngine@332 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

---
+++ Tools/TilePlacer/Constants.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GWNorthEngine.Tools.TilePlacer {
+	/// <summary>
+	/// Contains common constants for the TilePlacer tool
+	/// </summary>
+	public class Constants {
+		/// <summary>
+		/// File extension used by files for or created by the TilePlacer
+		/// </summary>
+		public const string FILE_EXTENSION = ".XML";
+		/// <summary>
+		/// Header that denotes the start of the map size section
+		/// </summary>
+		public const string XML_HEADER_SIZE = "Size";
+		/// <summary>
+		/// Height of the map
+		/// </summary>
+		public const string XML_HEIGHT = "Height";
+		/// <summary>
+		/// Width of the map
+		/// </summary>
+		public const string XML_WIDTH = "Width";
+		/// <summary>
+		/// Header that denotes the start of a layer
+		/// </summary>
+		public const string XML_HEADER_LAYER = "Layer";
+		/// <summary>
+		/// Header that denotes the start of a tile
+		/// </summary>
+		public const string XML_HEADER_TILE = "Tile";
+		/// <summary>
+		/// Name of the tile
+		/// </summary>
+		public const string XML_TILE_NAME = "Name";
+		/// <summary>
+		/// Filename of the tile
+		/// </summary>
+		public const string XML_TILE_FILE_NAME = "FileName";
+		/// <summary>
+		/// Value of the tile (TileValues int representation)
+		/// </summary>
+		public const string XML_TILE_VALUE = "Value";
+	}
+}
