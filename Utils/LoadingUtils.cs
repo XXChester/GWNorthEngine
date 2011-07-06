a0e8e1e74278954eab676ffb50164b971eadcd2e
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2011-07-06 00:11:59 +0000

    -Added in a way to load in textures not from the content pipeline. -Updated the project references doc to include the POC for no content project asset loading POC
    git-svn-id: http://gwnp-server/repos/Programming/Team%20Work/GWNorthEngine@375 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

--- Utils/LoadingUtils.cs
+++ Utils/LoadingUtils.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
@@ -14,8 +14,19 @@ namespace GWNorthEngine.Utils {
 	/// Contains helper methods for loading data from the file system
 	/// </summary>
 	public static class LoadingUtils {
-
 		#region Visual
+		/// <summary>
+		/// Sets all of the common data for the Texture2D
+		/// </summary>
+		/// <param name="texture">Loaded Texture2D to set the data on</param>
+		/// <param name="fileName">Name of the file</param>
+		/// <returns>Texture2D with the custom data appended to it</returns>
+		private static Texture2D setCommonAttributes(Texture2D texture, string fileName) {
+			fileName = StringUtils.scrubPathAndExtFromFileName(fileName);
+			texture.Name = fileName;
+			return texture;
+		}
+
 		/// <summary>
 		/// Loads a texture via the ContentManager and assigns the texture custom data for debugging purposes
 		/// </summary>
@@ -24,8 +35,60 @@ namespace GWNorthEngine.Utils {
 		/// <returns>Texture2D object</returns>
 		public static Texture2D loadTexture2D(ContentManager content, string textureName) {
 			Texture2D texture = content.Load<Texture2D>(textureName);
-			texture.Name = textureName;
-			return texture;
+			return (setCommonAttributes(texture, textureName));
+		}
+
+		/// <summary>
+		/// Loads a Texture2D not via the Content pipeline
+		/// </summary>
+		/// <param name="device">active GraphicsDevice reference</param>
+		/// <param name="fileNameWithPathAndExtension">Texute's name we are trying to load with the full path and file extension (Assets\Test.png)</param>
+		/// <returns>Texture2D object</returns>
+		/// <author>Largely based on http://jakepoz.com/jake_poznanski__speeding_up_xna.html </author>
+		public static Texture2D loadTexture2D(GraphicsDevice device, string fileNameWithPathAndExtension) {
+			Texture2D texture = null;
+			RenderTarget2D renderTarget = null;
+
+			using (Stream titleStream = TitleContainer.OpenStream(fileNameWithPathAndExtension)) {
+				texture = Texture2D.FromStream(device, titleStream);
+			}
+
+			//Setup a render target to hold our final texture which will have premulitplied alpha values
+			renderTarget = new RenderTarget2D(device, texture.Width, texture.Height);
+			device.SetRenderTarget(renderTarget);
+			device.Clear(Color.Black);
+
+			//Multiply each color by the source alpha, and write in just the color values into the final texture
+			BlendState blendColor = new BlendState();
+			blendColor.ColorWriteChannels = ColorWriteChannels.Red | ColorWriteChannels.Green | ColorWriteChannels.Blue;
+			blendColor.AlphaDestinationBlend = Blend.Zero;
+			blendColor.ColorDestinationBlend = Blend.Zero;
+			blendColor.AlphaSourceBlend = Blend.SourceAlpha;
+			blendColor.ColorSourceBlend = Blend.SourceAlpha;
+
+			// first pass render the texture to the RenderTarget with a blend state
+			SpriteBatch spriteBatch = new SpriteBatch(device);
+			spriteBatch.Begin(SpriteSortMode.Immediate, blendColor);
+			spriteBatch.Draw(texture, texture.Bounds, Color.White);
+			spriteBatch.End();
+
+			//Now copy over the alpha values from the PNG source texture to the final one, without multiplying them
+			BlendState blendAlpha = new BlendState();
+			blendAlpha.ColorWriteChannels = ColorWriteChannels.Alpha;
+			blendAlpha.AlphaDestinationBlend = Blend.Zero;
+			blendAlpha.ColorDestinationBlend = Blend.Zero;
+			blendAlpha.AlphaSourceBlend = Blend.One;
+			blendAlpha.ColorSourceBlend = Blend.One;
+
+			// second pass render the texture to the RenderTarget with an alpha state
+			spriteBatch.Begin(SpriteSortMode.Immediate, blendAlpha);
+			spriteBatch.Draw(texture, texture.Bounds, Color.White);
+			spriteBatch.End();
+
+			//Release the GPU back to drawing to the screen
+			device.SetRenderTarget(null);
+			texture = renderTarget;
+			return setCommonAttributes(texture, fileNameWithPathAndExtension);
 		}
 		
 		/// <summary>
---
+++ Utils/StringUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GWNorthEngine.Utils {
+	/// <summary>
+	/// Helper methods for manipulating string values
+	/// </summary>
+	public static class StringUtils {
+		/// <summary>
+		/// Stribs the file path and extension (if they exist) off of a file name
+		/// </summary>
+		/// <param name="fileName">filename to alter</param>
+		/// <returns>filename with the path and extension ripped off if they were present</returns>
+		public static string scrubPathAndExtFromFileName(string fileName) {
+			fileName = scrubPathFromFileName(fileName);
+			fileName = scrubExtensionFromFilename(fileName);
+			
+			return fileName;
+		}
+
+		/// <summary>
+		/// Scrubs the directory information off of a file name
+		/// </summary>
+		/// <param name="fileName">filename to alter</param>
+		/// <returns>filename with the path ripped off if it were present</returns>
+		public static string scrubPathFromFileName(string fileName) {
+			// do we need to scrub the directory information off?
+			if (fileName.Contains("\\")) {
+				fileName = fileName.Remove(0, fileName.LastIndexOf(@"\") + 1);
+			}
+			return fileName;
+		}
+		
+		/// <summary>
+		/// Scrubs the file extension from a file name
+		/// </summary>
+		/// <param name="fileName">filename to alter</param>
+		/// <returns>filename with the extension ripped off if it were present</returns>
+		public static string scrubExtensionFromFilename(string fileName) {
+			// do we need to scrub the file extension off
+			if (fileName.Contains(".")) {
+				fileName = fileName.Remove(fileName.IndexOf("."));
+			}
+			return fileName;
+		}
+	}
+}
